32997dce1b48b069f9a655065096dfe45a2e18a7
Alexandre <alexandre.toudic@bigyouth.fr>
2015-08-24 14:12:25 +0200

    update scroll 60fps

--- src/mixins/scroll60fps.js
+++ src/mixins/scroll60fps.js
@@ -3,19 +3,22 @@ export function scroll60fps() {
         body = document.body,
         timer;
 
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.scrolling, .scrolling * { pointer-events: none }';
+    document.querySelector('head').appendChild(style);
+
     window.addEventListener('scroll', function() {
       clearTimeout(timer);
 
       if(!scrolling) {
         scrolling = true;
         body.classList.add('scrolling');
-        body.style['pointer-events'] = 'none';
       }
 
       timer = setTimeout(function(){
         scrolling = false;
         body.classList.remove('scrolling');
-        body.style['pointer-events'] = '';
       }, 200);
     }, false);
 }
