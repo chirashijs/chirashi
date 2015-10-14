eb594f2001ccd92a8749ca6a6aa9f786fa7a8a9b
AlexToudic <alexandre.toudic@gmail.com>
2015-10-15 00:13:19 +0200

    core update

--- src/core/getElements.js
+++ src/core/getElements.js
@@ -1,10 +1,20 @@
+import { forEach } from './forEach';
 import { isDomElement } from './isDomElement';
 import { getSelectorAll } from './getSelectorAll';
 
 export function getElements (elements) {
     if (typeof elements == 'string') return getSelectorAll(elements);
 
-    if (elements instanceof Array || elements instanceof NodeList) return elements;
+    if (elements instanceof Array) {
+      let parsedElements = [];
+      forEach(elements, (element) => {
+        parsedElements = parsedElements.concat(getElements(element));
+      });
+
+      return parsedElements;
+    }
+
+    if (elements instanceof NodeList) return [].slice.call(elements);
 
     return isDomElement(elements) ? [elements] : null;
 }
