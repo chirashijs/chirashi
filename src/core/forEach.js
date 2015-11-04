42f07719137a9025838aeb1e3fc5d5568a0a8423
Alexandre <alexandre.toudic@bigyouth.fr>
2015-11-04 09:53:45 +0100

    forEach - add force order option

--- src/core/forEach.js
+++ src/core/forEach.js
@@ -2,14 +2,20 @@ import { getSelectorAll } from './getSelectorAll';
 import { getElement } from './getElement';
 import { isDomElement } from './isDomElement';
 
-export function forEach (elements, callback) {
+export function forEach (elements, callback, forceOrder = false) {
   if (!elements) return;
 
   if (!(elements instanceof Array || elements instanceof NodeList)) {
     callback(elements, 0);
   }
   else {
-    let i = elements.length;
-    while(i--) callback(elements[i], i);
+    if (!forceOrder) {
+        let i = elements.length;
+        while(i--) callback(elements[i], i);
+    }
+    else {
+        let i = -1;
+        while(++i < elements.length) callback(elements[i], i);
+    }
   }
 }
