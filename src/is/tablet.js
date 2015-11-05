df06332d3536f7c9f96e49c3321001bfcab56e2f
Alexandre <alexandre.toudic@bigyouth.fr>
2015-11-05 02:07:24 +0100

    fix tablet

--- src/is/tablet.js
+++ src/is/tablet.js
@@ -1,6 +1,6 @@
 import { isIPad } from './ipad';
 import { isAndroidTablet } from './androidTablet';
-import { isWindowsTablet } from './androidTablet';
+import { isWindowsTablet } from './windowsTablet';
 
 export function isTablet() {
   return isIPad() || isAndroidTablet() || isWindowsTablet();
