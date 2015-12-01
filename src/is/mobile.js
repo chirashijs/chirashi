872bcb96a74ffa571c19ff0b101df2172d9eaef2
Alex Toudic <alex@Alexs-MacBook-Pro.local>
2015-12-01 21:40:37 +0100

    isMobile fix

--- src/is/mobile.js
+++ src/is/mobile.js
@@ -2,6 +2,6 @@ import { isIOS } from './ios';
 import { isAndroid } from './android';
 import { isWindows } from './windows';
 
-export function isTablet() {
+export function isMobile() {
   return isIOS() || isAndroid() || (isWindows() && /touch/i.test(ua));
 }
