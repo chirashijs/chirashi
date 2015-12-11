62966b249cc5c14222daa4b0fa277fc7f92b2356
Alex Toudic <alex@Alexs-MacBook-Pro.local>
2015-12-11 18:23:57 +0100

    force order on forElements

--- src/core/forElements.js
+++ src/core/forElements.js
@@ -1,6 +1,6 @@
 import { forEach } from './forEach';
 import { getElements } from './getElements';
 
-export function forElements (elements, callback) {
-  forEach(getElements(elements), callback);
+export function forElements (elements, callback, forceOrder = false) {
+  forEach(getElements(elements), callback, forceOrder);
 }
