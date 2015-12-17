fb70b02ced3f3ec3d6d74b7b6939819c179c9b61
Alex Toudic <alex@Alexs-MacBook-Pro.local>
2015-12-18 00:04:47 +0100

    random between

--- src/utils/between.js
+++ src/utils/between.js
@@ -1,5 +1,5 @@
-export function between(value, min=0, max=1) {
-  return isNaN(value) ? max : Math.min(Math.max(value, min), max);
+export function between (max, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 export default between;
