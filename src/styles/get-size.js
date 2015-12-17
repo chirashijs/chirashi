436cd666a541e70f430e1747a190d0912ce6fc22
Alex Toudic <alex@Alexs-MacBook-Pro.local>
2015-12-17 11:44:47 +0100

    files fixes

--- src/styles/get-size.js
+++ src/styles/get-size.js
@@ -1,6 +1,6 @@
 import getElement from '../core/get-element';
-import getWidth from './getWidth';
-import getHeight from './getHeight';
+import getWidth from './get-width';
+import getHeight from './get-height';
 
 export function getSize (element) {
   element = getElement(element);
