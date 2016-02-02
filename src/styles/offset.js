3ba94275b4094d97eee81975fea1a99fba1ed37e
Alex Toudic <alex@Alexs-MacBook-Pro.local>
2016-02-02 19:27:56 +0100

    update offset

--- src/styles/offset.js
+++ src/styles/offset.js
@@ -7,8 +7,8 @@ export function offset (element) {
   let rect = element.getBoundingClientRect()
 
   return {
-    top: rect.top + document.body.scrollTop,
-    left: rect.left + document.body.scrollLeft
+    top: rect.top + (document.documentElement.scrollTop || document.body.scrollTop),
+    left: rect.left + (document.documentElement.scrollLeft || document.body.scrollLeft)
   }
 }
 
