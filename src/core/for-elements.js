54ec62c0cfbcd958d9bd26d1e6a3fb563fd662db
Alex Toudic <alex@Alexs-MacBook-Pro.local>
2016-02-03 10:42:37 +0100

    fix forElements duplicate

--- src/core/for-elements.js
+++ src/core/for-elements.js
@@ -1,8 +1,8 @@
 import forEach from './for-each'
 import { getElements } from './get-elements'
 
-export function forElements (elements, callback) {
-  forEach(getElements(elements), callback)
+export function forElements (elements, callback, forceOrder = false) {
+  forEach(getElements(elements), callback, forceOrder)
 }
 
 export default forElements
