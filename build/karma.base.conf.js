50309bdccafc7facd03cafc2414003944ae6f492
Alex Toudic <alexandre.toudic@gmail.com>
2017-01-19 09:43:46 +0100

    update karma

--- build/karma.base.conf.js
+++ build/karma.base.conf.js
@@ -1,10 +1,37 @@
-module.exports = {
-  basePath: '../',
+var path = require('path')
+var webpack = require('webpack')
 
-  files: ['../test/**/*.js'],
+var webpackConfig = {
+  resolve: {
+    alias: {
+      chirashi: path.resolve(__dirname, '../lib')
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel']
+      }
+    ]
+  }
+}
+
+module.exports = {
+  files: [
+    '../test/**/*.js'
+  ],
 
   preprocessors: {
     '../lib/**/*.js': ['webpack'],
     '../test/**/*.js': ['webpack']
+  },
+
+  webpack: webpackConfig,
+
+  webpackMiddleware: {
+    noInfo: true
   }
 }
